dcb19c335c5bfda7a687ce545f2a5213058355c1
sergiy khurtik <skhurtik@ukr.net>
2020-01-26 18:43:07 +0200

    Updates

---
+++ precache-manifest.c343197736b5adcb8e11c339bb18456a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fde5629ea3925ca675e336aa97653f7",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "cf1ce70d22c95ccac78d",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.447873d3.chunk.css"
+  },
+  {
+    "revision": "ec985b4737da5ed1461f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.0f64bbce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.0f64bbce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf1ce70d22c95ccac78d",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.223aa16d.chunk.js"
+  },
+  {
+    "revision": "063097635f1add7d9f2c",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.d3f3cb3b.js"
+  }
+]);
