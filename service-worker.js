0df42f8da431b403bbdfeda0e5a6bdfc9fde86f6
sergiy khurtik <skhurtik@ukr.net>
2020-01-29 22:53:57 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.bd664c1a1da64254ac62c851519b675b.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.3be7e955c06119ff4ddecb441760dcc9.js"
 );
 
 self.addEventListener('message', (event) => {
